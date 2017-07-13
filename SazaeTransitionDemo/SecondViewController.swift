52b295cff42e1b9a3730497edde84863b78a55dd
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2017-07-13 12:56:54 +0900

    Initial commit

---
+++ SazaeTransitionDemo/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  SazaeTransitionDemo
+//
+//  Created by Hirose.Yudai on 2017/07/13.
+//  Copyright © 2017年 Hirose.Yudai. All rights reserved.
+//
+
+import Foundation
---
+++ SazaeTransitionDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SazaeTransitionDemo
+//
+//  Created by Hirose.Yudai on 2017/07/13.
+//  Copyright © 2017年 Hirose.Yudai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
+
+class SecondViewController: UIViewController {
+    
+}
